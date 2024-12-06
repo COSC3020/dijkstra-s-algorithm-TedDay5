d4ffefdadd981f7286214a6218d7c8af75ae1c72
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-05 20:31:11 -0700

    Setting up code.test.js

--- code.test.js
+++ code.test.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+const assert = require('assert');
 
+eval(fs.readFileSync('code.js')+'');
+
+const graphA = [
+    
+]
+
+const graphB = [
+    
+]
+
+const graphSmallC = [
+    
+]
