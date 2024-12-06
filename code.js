34eded0256d166fe337b574af22d4db2d678d46f
TedDay5 <144728376+TedDay5@users.noreply.github.com>
2024-12-05 20:26:38 -0700

    implemented Dijkstra

--- code.js
+++ code.js
@@ -1,3 +1,35 @@
 function dijkstra(graph, sourceNode) {
-    return [];
+    if(Object.keys(graph).length < 2) {
+        return [];
+    }
+    let distance = {};
+    let unvisted = new Set(Object.keys(graph));
+
+    for(let node in graph) {
+        distance[node] = Infinity;
+    }
+    distance[sourceNode] = 0;
+    
+
+    while(unvisted.size > 0) {
+        let closeNode = null;
+        let minimum = Infinity;
+
+        for(let node of unvisted) {
+            if(distance[node] <= minimum) {
+                closeNode = node;
+                minimum = distance[node];
+            }
+        }
+        unvisted.delete(closeNode);
+        for(let neighbor in graph[closeNode]) {
+            let edgeWeight = graph[closeNode][neighbor];
+            let newDistance = distance[closeNode] + edgeWeight;
+
+            if(newDistance < distance[neighbor]) {
+                distance[neighbor] = newDistance;
+            }
+        }
+    }
+    return distance;
 }
